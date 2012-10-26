ed4a848df0f3e49d12c1589226027d05f14efa66
user <user@192.168.30.52>
2012-10-26 10:28:40 +0800

    小修改

--- jmail/test/com/huwewa/test/Main.java
+++ jmail/test/com/huwewa/test/Main.java
@@ -10,7 +10,7 @@ public class Main {
         String body = "亲爱的会员，您好，您在提交找回密码的请求。以下是您的帐户及密码信息：用户名：aa，密码：8523该密码是临时密码，请您尽快修改密码，感谢使用本系统此为自动发送邮件，请勿直接回复！"
                 + "请使用以下链接找回密码 : ";
 
-        Mail mail = new Mail(MailServerSetting.USER_NAME, "bj4312@163.com", subject, body);
+        Mail mail = new Mail(MailServerSetting.USER_NAME, "xxxx@163.com", subject, body);
         MailServer.send(mail);
     }
 }
